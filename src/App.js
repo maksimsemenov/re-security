f5207bedf727dc1f7fcabfabbd2faaa992995928
maksimsemenov <maksa.semenov@gmail.com>
2016-12-12 16:20:38 -0500

    Generating data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react'
 import Terminal from './Terminal'
-import { terminals } from './data'
+import { v4 } from 'uuid'
 import './App.css'
 
+const generateTerminals = () =>
+  new Array(200).fill(1).map(() => ({
+    guid: v4(),
+    attacks: new Array(90).fill(1).map(() => ({
+      file: Math.round(Math.random() * 20),
+      network: Math.round(Math.random() * 20),
+      registry: Math.round(Math.random() * 20),
+      process: Math.round(Math.random() * 20),
+      suspicios: Math.round(Math.random() * 20)
+    }))
+  }))
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { terminals: generateTerminals() }
+  }
+
+  generateData() {
+    this.setState({ terminals: generateTerminals() })
+  }
+
   render() {
+    const { terminals } = this.state
     return (
       <div className="App">
-        {terminals.map((terminal, index) =>
-          <Terminal key={index} {...terminal} />
-        )}
+        <button onClick={() => this.generateData()}>Generate data</button>
+        <div className='app__terminals'>
+          {terminals.map((terminal, index) =>
+            <Terminal key={index} {...terminal} />
+          )}
+        </div>
       </div>
     )
   }
--- src/Terminal.js
+++ src/Terminal.js
@@ -5,13 +5,13 @@ const Terminal = ({ guid, attacks }) => (
   <div className='terminal'>
     <div className='terminal__name'>{guid}</div>
     <div className='terminal__graph'>
-      {attacks.files.map((file, index) =>
+      {attacks.map((attack, index) =>
         <div key={index} className='terminal__bar'>
-          <div className='terminal__file' style={{ height: `${file/5}px`}}/>
-          <div className='terminal__network' style={{ height: `${attacks.network[index]/5}px`}}/>
-          <div className='terminal__registry' style={{ height: `${attacks.registry[index]/5}px`}}/>
-          <div className='terminal__process' style={{ height: `${attacks.process[index]/5}px`}}/>
-          <div className='terminal__suspicios' style={{ height: `${attacks.suspicios[index]/5}px`}}/>
+          <div className='terminal__file' style={{ height: `${attack.file}px`}}/>
+          <div className='terminal__network' style={{ height: `${attack.network}px`}}/>
+          <div className='terminal__registry' style={{ height: `${attack.registry}px`}}/>
+          <div className='terminal__process' style={{ height: `${attack.process}px`}}/>
+          <div className='terminal__suspicios' style={{ height: `${attack.suspicios}px`}}/>
         </div>
       )}
     </div>
